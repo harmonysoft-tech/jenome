38ec58af44f5514d8fa6b02b0f750b610169f3e4
Denis Zhdanov <dzhdanov@tango.me>
2017-11-17 22:23:10 +0300

    Revised GenericArrayTypeComplianceMatcherTest

--- src/test/java/tech/harmonysoft/oss/jenome/match/impl/GenericArrayTypeComplianceMatcherTest.java
+++ src/test/java/tech/harmonysoft/oss/jenome/match/impl/GenericArrayTypeComplianceMatcherTest.java
@@ -2,10 +2,11 @@ package tech.harmonysoft.oss.jenome.match.impl;
 
 import static junit.framework.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.Test;
 import tech.harmonysoft.oss.jenome.resolve.TypeArgumentResolver;
-import org.junit.Before;
-import org.junit.Ignore;
 
 import java.lang.reflect.GenericArrayType;
 import java.lang.reflect.ParameterizedType;
@@ -13,97 +14,84 @@ import java.lang.reflect.Type;
 import java.util.Collection;
 import java.util.List;
 
-@SuppressWarnings({"UnusedDeclaration"})
-// TODO den remove
-@Ignore
 public class GenericArrayTypeComplianceMatcherTest {
 
-    private GenericArrayTypeComplianceMatcher matcher;
-    private GenericArrayType numberArrayType = (GenericArrayType)
-            ((ParameterizedType) NumberArrayClass.class.getGenericInterfaces()[0]).getActualTypeArguments()[0];
-    private GenericArrayType objectArrayType = (GenericArrayType)
-            ((ParameterizedType) ObjectArrayClass.class.getGenericInterfaces()[0]).getActualTypeArguments()[0];
+    private GenericArrayTypeComplianceMatcher matcher = new GenericArrayTypeComplianceMatcher();
 
-    @Before
-    public void setUp() throws Exception {
-        matcher = new GenericArrayTypeComplianceMatcher();
+    private GenericArrayType collectionNumberArrayType = getType(CollectionNumberArrayClass.class);
+    private GenericArrayType objectArrayType           = getType(CollectionObjectArrayClass.class);
+
+    @NotNull
+    private static GenericArrayType getType(@NotNull Class<?> clazz) {
+        return (GenericArrayType) ((ParameterizedType) clazz.getGenericInterfaces()[0]).getActualTypeArguments()[0];
     }
 
-// TODO den fix
-//    @Test
+    @Test
     public void parameterizedType() {
-        assertFalse(matcher.match(numberArrayType, NumberArrayClass.class.getGenericInterfaces()[0]));
+        assertTrue(matcher.match(collectionNumberArrayType, getType(CollectionNumberArrayClass.class)));
+        assertTrue(matcher.match(collectionNumberArrayType, getType(ListNumberArrayClass.class)));
+        assertFalse(matcher.match(collectionNumberArrayType, getType(ListLongArrayClass.class)));
     }
 
-// TODO den fix
-//    @Test
+    @Test
     public void wildcardType() throws NoSuchFieldException {
         class TestClass {
-            public Collection<? extends Number> field1;
+            public Collection<? extends Number>   field1;
             public Collection<? extends Number[]> field2;
-            public Collection<? extends Long[]> field3;
-            public Collection<? super Long[]> field4;
-            public Collection<? super Number[]> field5;
-            public Collection<?> field6;
+            public Collection<? extends Long[]>   field3;
+            public Collection<? super Long[]>     field4;
+            public Collection<? super Number[]>   field5;
+            public Collection<?>                  field6;
         }
 
         Type lowerToNumber
-                = ((ParameterizedType)TestClass.class.getField("field1").getGenericType()).getActualTypeArguments()[0];
-        assertFalse(matcher.match(numberArrayType, lowerToNumber));
+                = ((ParameterizedType) TestClass.class.getField("field1").getGenericType()).getActualTypeArguments()[0];
+        assertFalse(matcher.match(collectionNumberArrayType, lowerToNumber));
         assertFalse(matcher.match(objectArrayType, lowerToNumber));
 
         Type lowerToNumberArray
-                = ((ParameterizedType)TestClass.class.getField("field2").getGenericType()).getActualTypeArguments()[0];
-        assertFalse(matcher.match(numberArrayType, lowerToNumberArray));
+                = ((ParameterizedType) TestClass.class.getField("field2").getGenericType()).getActualTypeArguments()[0];
+        assertFalse(matcher.match(collectionNumberArrayType, lowerToNumberArray));
         assertFalse(matcher.match(objectArrayType, lowerToNumberArray));
 
         Type lowerToLongArray
-                = ((ParameterizedType)TestClass.class.getField("field3").getGenericType()).getActualTypeArguments()[0];
-        assertFalse(matcher.match(numberArrayType, lowerToLongArray));
+                = ((ParameterizedType) TestClass.class.getField("field3").getGenericType()).getActualTypeArguments()[0];
+        assertFalse(matcher.match(collectionNumberArrayType, lowerToLongArray));
         assertFalse(matcher.match(objectArrayType, lowerToLongArray));
 
         Type upperToLongArray
-                = ((ParameterizedType)TestClass.class.getField("field4").getGenericType()).getActualTypeArguments()[0];
-        assertFalse(matcher.match(numberArrayType, upperToLongArray));
+                = ((ParameterizedType) TestClass.class.getField("field4").getGenericType()).getActualTypeArguments()[0];
+        assertFalse(matcher.match(collectionNumberArrayType, upperToLongArray));
         assertFalse(matcher.match(objectArrayType, upperToLongArray));
 
         Type upperToNumberArray
-                = ((ParameterizedType)TestClass.class.getField("field5").getGenericType()).getActualTypeArguments()[0];
-        assertFalse(matcher.match(numberArrayType, upperToNumberArray));
+                = ((ParameterizedType) TestClass.class.getField("field5").getGenericType()).getActualTypeArguments()[0];
+        assertFalse(matcher.match(collectionNumberArrayType, upperToNumberArray));
         assertFalse(matcher.match(objectArrayType, upperToNumberArray));
 
         Type wildcard
-                = ((ParameterizedType)TestClass.class.getField("field6").getGenericType()).getActualTypeArguments()[0];
-        assertFalse(matcher.match(numberArrayType, wildcard));
+                = ((ParameterizedType) TestClass.class.getField("field6").getGenericType()).getActualTypeArguments()[0];
+        assertFalse(matcher.match(collectionNumberArrayType, wildcard));
         assertFalse(matcher.match(objectArrayType, wildcard));
     }
 
-// TODO den fix
-//    @Test
+    @Test
     public void toGenericArray() throws NoSuchFieldException {
         class TestClass<T> {
-            public Collection<Integer[]> baseField1;
-            public Collection<Long[]> baseField2;
-            public Collection<List<? super Long>[]> baseField3;
-            public Collection<List<? extends Number[]>[]> baseField4;
+            public Collection<List<? super Long>[]>       baseField1;
+            public Collection<List<? extends Number[]>[]> baseField2;
 
-            public Collection<Integer> candidateField1;
-            public Collection<Object[]> candidateField2;
-            public Collection<Number[]> candidateField3;
-            public Collection<List<? super Number>[]> candidateField4;
-            public Collection<List<? super Integer>[]> candidateField5;
-            public Collection<List<Integer[]>[]> candidateField6;
+            public Collection<Number[]>                candidateField1;
+            public Collection<List<? super Number>[]>  candidateField2;
+            public Collection<List<? super Integer>[]> candidateField3;
+            public Collection<List<Integer[]>[]>       candidateField4;
         }
 
-        doTest(TestClass.class, "baseField1", "candidateField1", false);
-        doTest(TestClass.class, "baseField1", "candidateField2", false);
-        doTest(TestClass.class, "baseField2", "candidateField2", false);
+        doTest(TestClass.class, "baseField1", "candidateField2", true);
+        doTest(TestClass.class, "baseField1", "candidateField3", false);
+        doTest(TestClass.class, "baseField2", "candidateField1", false);
         doTest(TestClass.class, "baseField2", "candidateField3", false);
-        doTest(TestClass.class, "baseField3", "candidateField4", true);
-        doTest(TestClass.class, "baseField3", "candidateField5", false);
-        doTest(TestClass.class, "baseField4", "candidateField3", false);
-        doTest(TestClass.class, "baseField4", "candidateField5", false);
-        doTest(TestClass.class, "baseField4", "candidateField6", true);
+        doTest(TestClass.class, "baseField2", "candidateField4", true);
     }
 
     private void doTest(Class<?> clazz, String baseFieldName, String candidateFieldName, boolean expectedMatch)
@@ -118,28 +106,32 @@ public class GenericArrayTypeComplianceMatcherTest {
         return parameterizedType.getActualTypeArguments()[0];
     }
 
-// TODO den fix
-//    @Test
+    @Test
     public void typeVariable() {
         class TestClass<T> {}
-        assertFalse(matcher.match(numberArrayType, TestClass.class.getTypeParameters()[0]));
+        assertFalse(matcher.match(collectionNumberArrayType, TestClass.class.getTypeParameters()[0]));
         assertFalse(matcher.match(objectArrayType, TestClass.class.getTypeParameters()[0]));
     }
-// TODO den fix
-//    @Test
+
+    @Test
     public void toClass() {
-        assertFalse(matcher.match(numberArrayType, Number[].class));
+        assertFalse(matcher.match(collectionNumberArrayType, Number[].class));
         assertFalse(matcher.match(objectArrayType, Number[].class));
     }
 
-// TODO den fix
-//    @Test
+    @Test
     public void toType() {
-        assertFalse(matcher.match(numberArrayType, TypeArgumentResolver.RAW_TYPE));
+        assertFalse(matcher.match(collectionNumberArrayType, TypeArgumentResolver.RAW_TYPE));
         assertFalse(matcher.match(objectArrayType, TypeArgumentResolver.RAW_TYPE));
     }
 
     interface TestInterface<A> {}
-    class NumberArrayClass implements TestInterface<Number[]> {}
-    class ObjectArrayClass implements TestInterface<Object[]> {}
+
+    class CollectionNumberArrayClass implements TestInterface<Collection<Number>[]> {}
+
+    class ListNumberArrayClass implements TestInterface<List<Number>[]> {}
+
+    class ListLongArrayClass implements TestInterface<List<Long>[]> {}
+
+    class CollectionObjectArrayClass implements TestInterface<Collection<Object>[]> {}
 }
